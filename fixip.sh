371c683b2f0d1808856da448103b7e0c56b5ca42
remoteShell <onemedicine@users.noreply.github.com>
2019-07-30 15:25:29 +0800

    Update fixip.sh

--- fixip.sh
+++ fixip.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 IP=`curl http://members.3322.org/dyndns/getip`
-sed -i "s/\"name\":.*$/\"name\":\"$IP\",/" cfg.json
-sed -i "s/\"INSTANCE_NAME\":.*$/\"INSTANCE_NAME\":\"$IP\",/" cfg.json
+sed -i "s/\"name\":.*$/\"name\":\"$IP\",/" app.json
+sed -i "s/\"INSTANCE_NAME\":.*$/\"INSTANCE_NAME\":\"$IP\",/" app.json
